34f2d49c4045d13d530ffc7be0540072d8c6cda6
Gianni Bischoff <oucori@gmail.com>
2025-03-10 19:16:32 +0100

    feat: Add LockCommand for managing lock states via commands
    Introduces the "lock" command with "list" and "toggle" subcommands, allowing administrators to manage lock states directly in-game. Registers the command during the event for command registration, integrating it seamlessly into the server's functionality.

---
+++ src/main/kotlin/gg/wildblood/gearsofdiscovery/commands/LockCommand.kt
@@ -0,0 +1,75 @@
+package gg.wildblood.gearsofdiscovery.commands
+
+import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.arguments.StringArgumentType
+import com.mojang.brigadier.context.CommandContext
+import gg.wildblood.gearsofdiscovery.locks.ModRegistries.LOCK_REGISTRY_KEY
+import gg.wildblood.gearsofdiscovery.network.ModClientPayloadHandler
+import net.minecraft.ChatFormatting
+import net.minecraft.client.Minecraft
+import net.minecraft.commands.CommandSourceStack
+import net.minecraft.commands.Commands
+import net.minecraft.network.chat.ClickEvent
+import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.Style
+import kotlin.jvm.optionals.getOrNull
+
+
+object LockCommand {
+    fun register(dispatcher: CommandDispatcher<CommandSourceStack>) {
+        dispatcher.register(
+            Commands.literal("lock")
+                .requires { p -> p.hasPermission(4) }
+                .then(Commands.literal("list").executes { context -> listLocks(context); 0 })
+                .then(Commands.literal("toggle")
+                        .then(Commands.argument("lock", StringArgumentType.word())
+                            .executes { context -> toggleLock(context); 0 }
+                        )
+                )
+        )
+    }
+
+    private fun listLocks(context: CommandContext<CommandSourceStack>) {
+        val lockRegistry = Minecraft.getInstance().tryGetLockRegistry() ?: return
+
+        val messageText = Component.literal("Loaded Locks:")
+
+        lockRegistry.sortedBy { it.type }.forEach {
+            messageText.append(
+                Component.literal("\n- [${if(it.enabled) "✔" else "X"}] ${it.name}")
+                    .withStyle(if(it.enabled) ChatFormatting.GREEN else ChatFormatting.RED)
+                    .withStyle(Style.EMPTY.withClickEvent(ClickEvent(ClickEvent.Action.RUN_COMMAND, "/lock toggle ${it.name}")))
+            )
+        }
+
+        context.source.sendSuccess({ messageText },false)
+    }
+
+    private fun toggleLock(context: CommandContext<CommandSourceStack>) {
+        val lockName = context.getArgument("lock", String::class.java)
+
+        val lockRegistry = Minecraft.getInstance().tryGetLockRegistry() ?: return
+
+        val locks = lockRegistry.filter { it.name == lockName }
+
+        if (locks.isEmpty()) {
+            context.source.sendFailure(Component.literal("Lock $lockName not found!").withStyle(ChatFormatting.RED))
+            return
+        } else if (locks.size > 1) {
+            context.source.sendFailure(Component.literal("Multiple Locks with name $lockName found! ( i guess someone did a little fucky... )").withStyle(ChatFormatting.RED))
+        }
+
+        val lock = locks.first()
+
+        if (lock.enabled) {
+            ModClientPayloadHandler.serverAddUnlock(context.source.playerOrException, lock.name)
+        } else {
+            ModClientPayloadHandler.serverRemoveUnlock(context.source.playerOrException, lock.name)
+        }
+
+        context.source.sendSuccess({ Component.literal("Lock ${lock.name} ${if(lock.enabled) "disabled" else "enabled"}!") },false)
+
+    }
+}
+
+fun Minecraft.tryGetLockRegistry() = connection?.registryAccess()?.registry(LOCK_REGISTRY_KEY)?.getOrNull()
--- src/main/kotlin/gg/wildblood/gearsofdiscovery/events/ServerEvents.kt
+++ src/main/kotlin/gg/wildblood/gearsofdiscovery/events/ServerEvents.kt
@@ -2,10 +2,12 @@ package gg.wildblood.gearsofdiscovery.events
 
 import gg.wildblood.gearsofdiscovery.GearsOfDiscoveryMod
 import gg.wildblood.gearsofdiscovery.GearsOfDiscoveryMod.LOGGER
+import gg.wildblood.gearsofdiscovery.commands.LockCommand
 import gg.wildblood.gearsofdiscovery.network.ModClientPayloadHandler
 import net.minecraft.world.entity.player.Player
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.neoforge.event.RegisterCommandsEvent
 import net.neoforged.neoforge.event.entity.player.PlayerEvent
 
 @EventBusSubscriber(modid = GearsOfDiscoveryMod.MODID, bus = EventBusSubscriber.Bus.GAME)
@@ -17,5 +19,10 @@ object ServerEvents {
 
         ModClientPayloadHandler.updateUnlocksForPlayer(event.entity as Player)
 
+    }
+
+    @SubscribeEvent
+    fun onRegisterCommandsEvent(event: RegisterCommandsEvent) {
+        LockCommand.register(event.dispatcher)
     }
 }
